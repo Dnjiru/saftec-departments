2a5bbdf9b3a50c31c2fafe21fe5c75d13c181dd3
Mwaniki <mr.mwaniki@gmail.com>
2019-05-25 17:54:17 +0300

    test teardown and passed

--- src/test/java/dao/Sql2oTaskDaoTest.java
+++ src/test/java/dao/Sql2oTaskDaoTest.java
@@ -9,12 +9,15 @@ public class Sql2oTaskDaoTest {
   private Sql2oTaskDao taskDao; //ignore me for now. We'll create this soon.
   private Connection conn; //must be sql2o class conn
 
-  
-
-  @After
-  public void tearDown() throws Exception {
-    conn.close();
+  @Before
+  public void setUp() throws Exception {
+    String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+    Sql2o sql2o = new Sql2o(connectionString, "", "");
+    taskDao = new Sql2oTaskDao(sql2o); //ignore me for now
+    conn = sql2o.open(); //keep connection open through entire test so it does not get erased
   }
+
+  
 
   @Test
   public void addingTaskSetsId() throws Exception {
